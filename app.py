09bdfc3d7ae188067aaa8e0db98862c81b092c4b
Desmond Wong <d3smondwong@gmail.com>
2024-04-19 15:28:42 +0800

    update on app.py comments for predict() for html input

--- app.py
+++ app.py
@@ -48,6 +48,6 @@ def predict():
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True) # to debug in future
    
      
